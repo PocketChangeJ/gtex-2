3ae00b2bf2fafd2d0b3ef402e418d1da4b6ab3a8
TR <git@fin0c.net>
2019-03-28 23:47:50 -0500

    Initial commit

---
+++ config.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+## file: config.sh
+## desc: Contains configuration variables, such as data directories and commonly used
+##       functions, for the shell scripts in this repo.
+
+logger() { printf "[%s] %s\n" "$(date '+%Y.%m.%d %H:%M:%S')" "$*" >&2; }
+
+## Retruns the directory that contains config.sh regardless of where the script is
+## executed
+SELF_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+## Source directory
+SRC_DIR="$SELF_DIR/src"
+## Data directory
+DATA_DIR="$SELF_DIR/data"
+
+DIRS=($DATA_DIR)
+
+for d in "${DIRS[@]}"
+do
+    [[ ! -f "$d" ]] && mkdir -p "$d";
+done
+
